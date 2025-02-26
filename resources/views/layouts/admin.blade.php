d840fa26e4ebd90bddc28477e8b064c21bd31473
PABLOILYAS <155155104+PABLOILYAS@users.noreply.github.com>
2025-02-26 19:33:44 +0100

    Updated Info

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -159,7 +159,7 @@
                     <strong><i class="fa fa-fw {{ $appIsGit ? 'fa-git-square' : 'fa-code-fork' }}"></i></strong> {{ $appVersion }}<br />
                     <strong><i class="fa fa-fw fa-clock-o"></i></strong> {{ round(microtime(true) - LARAVEL_START, 3) }}s
                 </div>
-                Copyright &copy; 2022 - {{ date('Y') }} <a href="https://nookure.com/">Nookure</a>.
+                Copyright &copy; 2022 - {{ date('Y') }} <a href="https://pabloilyas.servegame.com/">PABLOHOSTING</a>.
             </footer>
         </div>
         @section('footer-scripts')
